b579173340588fe2e40bf361602336f43cd6061c
Venkatesh Pediredla <venkatesh.pediredla@bt.com>
2020-04-26 12:42:11 +0530

    Preparing for Dynamic Content Commit

--- jobs/app.py
+++ jobs/app.py
@@ -7,8 +7,9 @@ app = Flask(__name__)
 def open_connection():
     connection = getattr(g,'_connection',None)
     if(connection == None):
-        connection.row_factory = sqlite3.Row
         connection,g._connection = sqlite3.connect(PATH)
+
+    connection.row_factory = sqlite3.Row
     return connection
 
 def execute_sql(sql,values=(),commit=False,single=False):
