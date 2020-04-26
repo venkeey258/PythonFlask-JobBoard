a77dd41d57cf88d7e616772a79bd81a5b26e8ea0
Venkatesh Pediredla <venkatesh.pediredla@bt.com>
2020-04-27 00:34:14 +0530

    Final Commit-1

--- jobs/app.py
+++ jobs/app.py
@@ -61,7 +61,7 @@ def review(employer_id):
         execute_sql('INSERT INTO review (review, rating, title, date, status, employer_id) VALUES (?, ?, ?, ?, ?, ?)',
                     (review, rating, title, date, status, employer_id),
                     commit=True)
-        redirect(url_for("employer",employer_id=employer_id))
+        return redirect(url_for("employer",employer_id=employer_id))
 
     return render_template("review.html", employer_id=employer_id)
 
